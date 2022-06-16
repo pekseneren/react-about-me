f21ac06b1586851d8dd63ebc4faaef7cf9cd897e
Eren Pekşen <peksen.eren@gmail.com>
2022-06-17 00:18:38 +0300

    Update Home.js

--- src/components/base_components/Home.js
+++ src/components/base_components/Home.js
@@ -1,11 +1,10 @@
-import { Card, Container, CardContent } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import '../../App.css'
 import { Link } from "react-router-dom";
 import github from "super-tiny-icons/images/svg/github.svg";
 import stackoverflow from "super-tiny-icons/images/svg/stackoverflow.svg";
 import linkedin from "super-tiny-icons/images/svg/linkedin.svg";
 import twitter from "super-tiny-icons/images/svg/twitter.svg";
-import instagram from "super-tiny-icons/images/svg/instagram.svg";
 import gmail from "super-tiny-icons/images/svg/gmail.svg";
 
 function Home() {
@@ -16,8 +15,6 @@ function Home() {
                 <div className="m-5 text-white">
                     <p>Hello World 👋</p>
                     <p>I'm Eren(not the titan), I am a backend developer living in Istanbul ❤️</p>
-                    <p>Heres some technologies I have used 👉 .NET, JavaScript, Git, MSSQL, SQLite, Redis and Unity</p>
-                    <p>And these are the technologies I want to understand completely and start learning in 2021 👉 Go, MongoDB, PostgreSQL, ReactJS, NodeJS, GraphQL, Docker, Kubernetes and Kafka</p>
                     <Link to="/react-about-me/projects">my github projects</Link>
                     <div className="mt-5 mx-auto">
                         <a href="https://github.com/pekseneren" target="blank">
@@ -31,9 +28,6 @@ function Home() {
                         </a>
                         <a href="https://twitter.com/pekseneren" target="blank">
                             <img className="social-link" alt="twitter" src={twitter} />
-                        </a>
-                        <a href="https://www.instagram.com/peksen.eren" target="blank">
-                            <img className="social-link" alt="instagram" src={instagram} />
                         </a>
                         <a href="mailto:peksen.eren@gmail.com">
                             <img className="social-link" alt="gmail" src={gmail} />
