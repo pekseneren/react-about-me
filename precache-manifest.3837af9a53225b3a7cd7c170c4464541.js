85f969e937cc9e4c3c3ac0205e38ac56f0e29609
Eren Pekşen <peksen.eren@gmail.com>
2023-04-30 16:43:57 +0100

    Updates

---
+++ precache-manifest.3837af9a53225b3a7cd7c170c4464541.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "466c7b808e7e85d6f551",
+    "url": "/react-about-me/static/css/main.3ba9c269.chunk.css"
+  },
+  {
+    "revision": "466c7b808e7e85d6f551",
+    "url": "/react-about-me/static/js/main.466c7b80.chunk.js"
+  },
+  {
+    "revision": "92682ea5af0201209f7d",
+    "url": "/react-about-me/static/js/1.92682ea5.chunk.js"
+  },
+  {
+    "revision": "53e24dd0750f7924d72d",
+    "url": "/react-about-me/static/js/2.53e24dd0.chunk.js"
+  },
+  {
+    "revision": "f31e7fd2e9b552547aa3",
+    "url": "/react-about-me/static/js/runtime~main.f31e7fd2.js"
+  },
+  {
+    "revision": "46022f307d3f479cf386915d9a308034",
+    "url": "/react-about-me/static/media/github.46022f30.svg"
+  },
+  {
+    "revision": "a4eeb5defd48d3d4f90012bd91f84844",
+    "url": "/react-about-me/static/media/stackoverflow.a4eeb5de.svg"
+  },
+  {
+    "revision": "65f4f208eed538fb009e45f9ffba6c64",
+    "url": "/react-about-me/static/media/linkedin.65f4f208.svg"
+  },
+  {
+    "revision": "dc4fff597333068055d87f91eee015a0",
+    "url": "/react-about-me/index.html"
+  }
+];
